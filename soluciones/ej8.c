0055cd2b7092af32cf48c1b13c8e2899bfdbb0b0
Ramirez Olvera Erick Yael <ypollo20@gmail.com>
2026-02-20 23:05:10 -0600

    Update ej8.c

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,20 @@
+base = 2000
+sizeof(int)=4
+m[1][2] = 2024
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 8
-    return 0;
-}
+/*
+ 1.Número de columnas
+ &m[1][2] = 2000 + (i * N + j) * sizeof(int)
+  &m[1][2] = 2000 + (1 * N + 2) *4
+  (2024 - 2000) / 4 = N + 2
+  N = 6 - 2
+  N = 4
+  
+ 2.Tamaño total
+ Fila = 0 y 1, osea 2 filas
+ Columnas = 4
+Entonces:
+2 * 4 = 8
+8 * sizeof(int)
+8 * 4 = 32 bytes
+*/ 
