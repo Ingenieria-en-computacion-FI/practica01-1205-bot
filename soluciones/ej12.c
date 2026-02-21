d15847b8136d79e0c70faf829acf3818a2d1d682
Ramirez Olvera Erick Yael <ypollo20@gmail.com>
2026-02-20 23:02:24 -0600

    Update ej12.c

--- soluciones/ej12.c
+++ soluciones/ej12.c
@@ -1,6 +1,21 @@
+int m[3][5];
+base = 4000;
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 12
-    return 0;
-}
+/*
+ formula:
+    &m[i][j] = base + (i * N + j) * sizeof(tipo_de_dato)
+    N = numero de columnas N = j
+    int = 4 bytes
+    
+ 1.Dirección de m[2][4]
+    &m[2][4] = 4000 + (2 * 5 + 4) * 4
+    &m[2][4] = 4056
+    
+ 2.Dirección de m[1][3]
+    &m[2][4] = 4000 + (1 * 5 + 3) * 4
+    &m[2][4] = 4032
+
+ 3.Diferencia entre ambas:
+    &m[2][4] - &m[2][4]
+    4056 - 4032 = 24
+*/ 
