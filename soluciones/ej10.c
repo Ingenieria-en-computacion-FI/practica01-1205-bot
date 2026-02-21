468790c7aad3178a830293e6b2cb35e277df125c
Ramirez Olvera Erick Yael <ypollo20@gmail.com>
2026-02-20 23:01:23 -0600

    Update ej10.c

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -1,6 +1,17 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 10
-    return 0;
+
+int gigante[10000000];
+
+int main() {
+
+    /* 
+    El arreglo no puede ponerse en la parte del main porque seria
+    una declaracion local y como el arreglo es muy grande debe declararse a fuera 
+    osea tienes que declararla como una funcion global 
+    las variables globales se almacenan en BSS/DATA, para 
+    el Stack o pila debe ir en el main porque seria una variable 
+    local y aqui podria ir si fuera mas pequeña 
+    
+    */
+
 }
