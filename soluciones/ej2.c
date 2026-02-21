f046642c239ac3c8253712e62d29879b6df6575b
Ramirez Olvera Erick Yael <ypollo20@gmail.com>
2026-02-20 23:02:43 -0600

    Update ej2.c

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,18 @@
+/*
+ 1.Cuál dirección es mayor:
+   &a[4] o &a[5]
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 2
-    return 0;
-}
+formula: 
+  &a[i] = base + i * sizeof(tipo_de_dato)
+      base = B
+      sizeof(tipo_de_dato) = d
+
+&a[4] = B + 4 * d
+&a[5] = B + 5 * d
+
+Comparacion: 
+      (B + 5 * d) - (B + 4 * d)
+      
+como el resultado debe ser postivo puedo afirmar que:
+&a[5] > &a[4]
+*/
